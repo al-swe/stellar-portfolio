f9f4105190bda7ce4ccdcf216ea45e3d5eaaa0f1
al-sw <alanjunior.sweeney@gmail.com>
2024-10-20 00:11:50 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,6 @@
+function submitForm(event) {
+    event.preventDefault();
+
+    document.getElementById('form-container').style.display = 'none';
+    document.getElementById('thankYouContainer').style.display = 'block';
+}
